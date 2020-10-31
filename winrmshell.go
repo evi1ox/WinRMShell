c94378329cb270d52d1f1ab694ee475d974910cb
evi1ox <dropboy@qq.com>
2020-10-31 12:07:54 +0800

    Add batch

--- winrmshell.go
+++ winrmshell.go
@@ -7,19 +7,24 @@ import (
 	"flag"
 	"fmt"
 	"github.com/evi1ox/WinRMShell/winrmcp"
+	"github.com/malfunkt/iprange"
 	"log"
+	"net"
 	"os"
 	_ "reflect"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/masterzen/winrm"
 	"github.com/mattn/go-isatty"
+	"github.com/gookit/color"
 )
 
 func main() {
 	var (
-		hostname string
+		hostlists string
 		user     string
 		pass     string
 		ntlm     bool
@@ -32,23 +37,45 @@ func main() {
 		timeout  string
 		src_path string
 		dst_path string
+		err            error
+		connectTimeout time.Duration
 	)
 
-	flag.StringVar(&hostname, "i", "127.0.0.1", "WinRM Host")
+	flag.StringVar(&hostlists, "i", "", "Host to be scanned, supports four formats:\n192.168.1.1\n192.168.1.1-10\n192.168.1.*\n192.168.1.0/24.")
 	flag.StringVar(&user, "u", "administrator", "WinRM Username")
 	flag.StringVar(&pass, "p", "password", "WinRM Password")
 	flag.BoolVar(&ntlm, "ntlm", false, "Use NTLM")
 	flag.BoolVar(&encoded, "encoded", false, "Use Base64 Encoded Password")
 	flag.BoolVar(&shell,"shell",false,"Use Interactive Shell Terminal")
-	flag.IntVar(&port, "port", 5985, "WinRM Port")
+	flag.IntVar(&port, "P", 5985, "WinRM Port")
 	flag.BoolVar(&https, "https", false, "Use Https")
 	flag.BoolVar(&insecure, "insecure", true, "Skip SSL Validation")
-	flag.StringVar(&timeout, "timeout", "0s", "Connection Timeout")
+	flag.StringVar(&timeout, "t", "2", "Connection Timeout")
 	flag.StringVar(&cmd, "cmd", "whoami", "Run Command Exec")
 	flag.StringVar(&src_path, "src", "", "Source Address")
 	flag.StringVar(&dst_path, "dst", "", "Destination address")
 
 	flag.Parse()
+
+	os.Setenv("WINRMCP_DEBUG","yes")
+
+	hostsPattern := `^(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])\/(\d{1}|[0-2]{1}\d{1}|3[0-2])$|^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$`
+	hostsRegexp := regexp.MustCompile(hostsPattern)
+	checkHost := hostsRegexp.MatchString(hostlists)
+
+	hostsPattern2 := `\b(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})\-((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2}))\b`
+	hostsRegexp2 := regexp.MustCompile(hostsPattern2)
+	checkHost2 := hostsRegexp2.MatchString(hostlists)
+
+	hostsPattern3 := `((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(\*$)`
+	hostsRegexp3 := regexp.MustCompile(hostsPattern3)
+	checkHost3 := hostsRegexp3.MatchString(hostlists)
+
+	if hostlists == "" || (checkHost == false && checkHost2 == false && checkHost3 == false){
+		flag.Usage()
+		return
+	}
+
 
 	if encoded {
 		data, err := base64.StdEncoding.DecodeString(pass)
@@ -56,37 +83,56 @@ func main() {
 		pass = strings.TrimRight(string(data), "\r\n")
 	}
 
-	var (
-		err            error
-		connectTimeout time.Duration
-	)
-
+	timeout += "s"
 	connectTimeout, err = time.ParseDuration(timeout)
 	check(err)
 
-	if src_path != "" && dst_path !=""{
-		addr := fmt.Sprintf("%s:%d", hostname,port)
-		client, err := winrmcp.New(addr, &winrmcp.Config{
-			Auth:                  winrmcp.Auth{User: user, Password: pass},
-			Https:                 https,
-			Insecure:              insecure,
-			TLSServerName:         "",
-			CACertBytes:           nil,
-			OperationTimeout:      time.Second*60,
-			MaxOperationsPerShell: 15,
-		})
+	hostlist, err := iprange.ParseList(hostlists)
+	check(err)
+	hostlistExpand := hostlist.Expand()
+	fmt.Printf("[+] 共执行指令的服务器个数: %d\n",len(hostlistExpand))
 
-		if err != nil {
-			log.Fatal(err)
+	if len(hostlistExpand) != 1 {
+		shell = false
+	}
+
+	for _,hostname := range hostlistExpand {
+		hostname := hostname.String()
+
+		color.Red.Printf("\n[+] %s",hostname)
+
+		if connect := raw_connect(hostname, strconv.Itoa(port),connectTimeout); connect != true {
+			fmt.Println("    连接失败")
+			continue
 		}
-		client.Copy(src_path, dst_path)
 
-	}else{
-		command(hostname,port,user,pass,https,cmd,insecure,connectTimeout,shell,ntlm)
+		if src_path != "" && dst_path !=""{
+			fmt.Println()
+			addr := fmt.Sprintf("%s:%d", hostname,port)
+			client, err := winrmcp.New(addr, &winrmcp.Config{
+				Auth:                  winrmcp.Auth{User: user, Password: pass},
+				Https:                 https,
+				Insecure:              insecure,
+				TLSServerName:         "",
+				CACertBytes:           nil,
+				OperationTimeout:      connectTimeout,
+				MaxOperationsPerShell: 15,
+			})
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			client.Copy(src_path, dst_path)
+
+		}else{
+			command(hostname,port,user,pass,https,cmd,insecure,connectTimeout,shell,ntlm)
+			//exitCode:=command(hostname,port,user,pass,https,cmd,insecure,connectTimeout,shell,ntlm)
+			//fmt.Println(exitCode)
+		}
 	}
 }
 
-func command(hostname string ,port int,user string, pass string, https bool,cmd string, insecure bool, connectTimeout time.Duration, shell bool, ntlm bool){
+func command(hostname string ,port int,user string, pass string, https bool,cmd string, insecure bool, connectTimeout time.Duration, shell bool, ntlm bool)(exitCode int){
 	endpoint := winrm.NewEndpoint(hostname, port, https, insecure, nil, nil, nil, connectTimeout)
 
 	params := winrm.DefaultParameters
@@ -98,19 +144,20 @@ func command(hostname string ,port int,user string, pass string, https bool,cmd 
 	client, err := winrm.NewClientWithParameters(endpoint, user, pass, params)
 	check(err)
 
-	exitCode := 0
+
+
 	if shell {
 		reader := bufio.NewReader(os.Stdin)
 		for {
-			fmt.Print("$ ")
+			fmt.Print("\n$ ")
 			cmdString, err := reader.ReadString('\n')
 			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
+				log.Println(os.Stderr, err)
 			}
 
 			cmdString = strings.TrimSuffix(cmdString, "\n")
 			if cmdString == "exit"{
-				os.Exit(exitCode)
+				return exitCode
 			}
 
 			if isatty.IsTerminal(os.Stdin.Fd()) {
@@ -120,20 +167,23 @@ func command(hostname string ,port int,user string, pass string, https bool,cmd 
 			}
 
 			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
+				log.Println(os.Stderr, err)
 			}
 		}
 	}else {
+		fmt.Printf("\n    ")
 		if isatty.IsTerminal(os.Stdin.Fd()) {
 			exitCode, err = client.Run(cmd, os.Stdout, os.Stderr)
 		} else {
 			exitCode, err = client.RunWithInput(cmd, os.Stdout, os.Stderr, os.Stdin)
 		}
-		check(err)
+		if err !=nil{
+			log.Println(err)
+		}
 	}
+	return exitCode
 
-
-	os.Exit(exitCode)
+	//os.Exit(exitCode)
 }
 
 func check(err error) {
@@ -141,4 +191,15 @@ func check(err error) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func raw_connect(host string, port string, timeout time.Duration) bool {
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+	if err != nil {
+		return false
+	}
+	if conn != nil {
+		defer conn.Close()
+	}
+	return true
 }
